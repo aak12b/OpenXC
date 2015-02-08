da8d59e74a4bd541f2eb5879335cda55fc9dda8b
Aaron <aak12b@my.fsu.edu>
2015-02-08 06:07:53 -0500

    All basic functionality complete. UI changes to accomodate change in function. Added file storage for profile and data logging. fixed all crashing from generating empty reports. Many reports can be generated, only store most recent. I'm the best. Down with Jared.

--- openxcstarter/src/main/java/com/openxc/openxcstarter/StarterActivity.java
+++ openxcstarter/src/main/java/com/openxc/openxcstarter/StarterActivity.java
@@ -2,8 +2,11 @@ package com.openxc.openxcstarter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,13 @@ import com.openxc.VehicleManager;
 import com.openxcplatform.openxcstarter.MyIntentService;
 import com.openxcplatform.openxcstarter.R;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
 
 public class StarterActivity extends Activity {
 
@@ -21,11 +31,16 @@ public class StarterActivity extends Activity {
 
 
     public Button button;
+    public Button grbutton;
+    public Button pButton;
     public String phoneNo;
     public String message;
     public int max_speed;
     public int max_rpms;
     public double max_kph;
+    public String file = "reportFile";
+    public String profile = "profile";
+    public String profiledata = "";
 
 
 
@@ -35,6 +50,8 @@ public class StarterActivity extends Activity {
         setContentView(R.layout.activity_starter);
 
         addListener();
+        reportListener();
+        profListener();
 
     }
 
@@ -50,13 +67,28 @@ public class StarterActivity extends Activity {
                 max_speed = Integer.parseInt(speed.getText().toString());
                 max_kph = max_speed * 1.609;
                 max_rpms = Integer.parseInt(rpms.getText().toString());
-                message = "Vehicle has gone faster than " + max_speed + " miles per hour.";
-                String intro = "This number has been chosen as a VT Phone Home Guardian. The maximum MPH of the vehicle was entered as "
-                        + max_speed + ".";
-                Intent i = new Intent(StarterActivity.this, MyIntentService.class);
-                String push = max_kph + ":" + phoneNo + ":" + max_rpms;
-                i.putExtra("txt", push);
-                startService(i);
+
+                //Begin profile creation
+                profiledata = max_kph + ":" + phoneNo + ":" + max_rpms;
+                File makeprofile = new File(getFilesDir(), profile);
+                FileOutputStream os;
+                try {
+                    os = new FileOutputStream(makeprofile);
+                    //os.write(string.getBytes());
+                    os.write(profiledata.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //End Profile Creation
+
+                //message = "Vehicle has gone faster than " + max_speed + " miles per hour.";
+                String intro = "This number has been chosen as a VT Phone Home Guardian.";
+               // Intent i = new Intent(StarterActivity.this, MyIntentService.class);
+                //String push = max_kph + ":" + phoneNo + ":" + max_rpms;
+                //String push = profiledata;
+                //i.putExtra("txt", push);
+                //startService(i);
                 try {
                     SmsManager smsManager = SmsManager.getDefault();
                     smsManager.sendTextMessage(phoneNo, null, intro, null, null);
@@ -67,6 +99,86 @@ public class StarterActivity extends Activity {
                             Toast.LENGTH_LONG).show();
                     e.printStackTrace();//
                 }//
+            }
+        });
+    }
+
+    public void profListener() {
+        pButton = (Button) findViewById(R.id.useProfile);
+        pButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileInputStream fis;
+                profiledata = "";
+                try {
+                    fis = openFileInput(profile);
+                    byte[] input = new byte[fis.available()];
+                    while (fis.read(input) != -1) {}
+                    profiledata += new String(input);
+                    Toast.makeText(getApplicationContext(),
+                            "Using Stored Profile",
+                            Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(StarterActivity.this, MyIntentService.class);
+                    String push = profiledata;
+                    i.putExtra("txt", push);
+                    startService(i);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "No Profile Stored",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public void reportListener(){
+        grbutton = (Button) findViewById(R.id.generateForm);
+        grbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileInputStream pGrabber;
+                profiledata = "";
+                try {
+                    pGrabber = openFileInput(profile);
+                    byte[] input = new byte[pGrabber.available()];
+                    while (pGrabber.read(input) != -1) {}
+                    profiledata += new String(input);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                FileInputStream fis;
+                String content = "";
+                try {
+                    fis = openFileInput(file);
+                    byte[] input = new byte[fis.available()];
+                    while (fis.read(input) != -1) {}
+                    content += new String(input);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String[] parsed = profiledata.split(":");
+                phoneNo = parsed[1];
+
+                Toast.makeText(getApplicationContext(),
+                        "Report Sent",
+                        Toast.LENGTH_LONG).show();
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNo, null, content, null, null);
+
+                //File filex = new File(getFilesDir(), file);
+                //filex.delete();
             }
         });
     }
--- openxcstarter/src/main/java/com/openxcplatform/openxcstarter/MyIntentService.java
+++ openxcstarter/src/main/java/com/openxcplatform/openxcstarter/MyIntentService.java
@@ -11,10 +11,15 @@ import android.util.Log;
 
 import com.openxc.VehicleManager;
 import com.openxc.measurements.EngineSpeed;
+import com.openxc.measurements.FuelLevel;
 import com.openxc.measurements.Measurement;
 import com.openxc.measurements.VehicleSpeed;
 import com.openxc.units.KilometersPerHour;
+import com.openxc.units.Percentage;
 import com.openxc.units.RotationsPerMinute;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 public class MyIntentService extends IntentService {
@@ -27,14 +32,27 @@ public class MyIntentService extends IntentService {
     private double redline;
     private double vspeed;
     private double rpms;
+    private double pfuel;
     private double revThreshold = 0.75; //75% threshold from redline
+    private double highSpeed = 0;
+    private double highRevs = 0;
     private String message;
     private String phoneNo;
     private String busted = "The vehicle has exceeded the indicated maximum speed.";
     private String bustedHR = "The vehicle has exceeded 75% redline.";
+    private String halfFuel = "The vehicle has less than 1/2 tank of fuel remaining.";
+    private String quarterFuel = "The vehicle has less than 1/4 tank of fuel remaining.";
+    private String gasmessage;
+    private boolean caughtRevving = false;
+    private boolean caughtSpeeding = false;
+    private boolean HFnoted = false;
+    private boolean QFnoted = false;
+    String filename = "reportFile";
+    String filetest = "Testing the file";
+    String finalOut = "";
 
-    private int VScount = 0;
-    private int RevCount = 0;
+    //private int VScount = 0;
+    //private int RevCount = 0;
 
     public MyIntentService() {
         super("MyIntentService");
@@ -59,12 +77,16 @@ public class MyIntentService extends IntentService {
                     KilometersPerHour derp = speed.getValue();
                     vspeed = derp.doubleValue();
                     // do stuff with the measurement
-                    if (vspeed > max_kph && VScount == 0)
+                    if (vspeed > highSpeed)
+                        highSpeed = vspeed;
+                    if (vspeed > max_kph && !caughtSpeeding)
                     {
                         SmsManager smsManager = SmsManager.getDefault();
                         smsManager.sendTextMessage(phoneNo, null, busted, null, null);
-                        work = false;
-                        VScount++;
+                        if (caughtRevving && (HFnoted || QFnoted))
+                            work = false;
+
+                        caughtSpeeding = true;
                     }
                 }
             };
@@ -74,18 +96,45 @@ public class MyIntentService extends IntentService {
                     final EngineSpeed revs = (EngineSpeed) measurement;
                     RotationsPerMinute rotes = revs.getValue();
                     rpms = rotes.doubleValue();
-
-                    if (rpms > max_revs && RevCount == 0){
+                    if (rpms > highRevs)
+                        highRevs = rpms;
+                    if (rpms > max_revs && !caughtRevving){
                         SmsManager smsManager = SmsManager.getDefault();
                         smsManager.sendTextMessage(phoneNo, null, bustedHR, null, null);
-                        work = false;
-                        RevCount++;
+                        if (caughtSpeeding && (HFnoted || QFnoted))
+                            work = false;
+                        caughtRevving = true;
                     }
+                }
+            };
+
+            FuelLevel.Listener flListener = new FuelLevel.Listener() {
+                public void receive(Measurement measurement) {
+                    final FuelLevel fuelremn = (FuelLevel) measurement;
+                    Percentage perFuel = fuelremn.getValue();
+                    pfuel = perFuel.doubleValue();
+
+                    if (pfuel < 25 && !QFnoted) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo, null, quarterFuel, null, null);
+                        QFnoted = true;
+                        if (caughtRevving && caughtSpeeding)
+                            work = false;
+                    }
+                    else if (pfuel < 50 && !HFnoted){
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(phoneNo, null, halfFuel, null, null);
+                        HFnoted = true;
+                        if (caughtRevving && caughtSpeeding)
+                            work = false;
+                    }
+
                 }
             };
 
             mVehicleManager.addListener(VehicleSpeed.class, listener);
             mVehicleManager.addListener(EngineSpeed.class, esListener);
+            mVehicleManager.addListener(FuelLevel.class, flListener);
         }
 
         // Called when the connection with the service disconnects unexpectedly
@@ -116,15 +165,60 @@ public class MyIntentService extends IntentService {
         redline = Double.parseDouble(separated[2]);
         max_revs = revThreshold * redline;
 
+
         while(work) {
             //Dont do a damn thing.
         }
+        highRevs = Math.ceil(highRevs);
+        highSpeed = Math.ceil(highSpeed / 1.609);
 
+        finalOut = "Latest Report \nHighest Speed: ";
+        finalOut += String.valueOf(highSpeed) + " mph";
+        finalOut += "\nFuel Level: ";
+        finalOut += String.valueOf(pfuel) + "%";
+        finalOut += "\nHighest RPMs: ";
+        finalOut += String.valueOf(highRevs);
+        finalOut += "\n";
 
+        Log.i(TAG, "File write code below");
+        File filex = new File(getFilesDir(), filename);
+        if (filex.exists())
+            filex.delete();
+
+        FileOutputStream os;
+        try {
+            os = new FileOutputStream(filex, true);
+            //os.write(string.getBytes());
+            os.write(finalOut.getBytes());
+            os.close();
+            Log.i(TAG, "File write attempt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onDestroy(){
+        //Testing file write for generate report.
+/*        Log.i(TAG, "File write code below");
+        File filex = new File(getFilesDir(), filename);
+        FileOutputStream os;
+        try {
+            os = new FileOutputStream(filex, true);
+            //os.write(string.getBytes());
+            os.write(filetest.getBytes());
+            os.close();
+            Log.i(TAG, "File write attempt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //if file does not exist, create file
+        //else add to file
+                //time of data added (trip?)
+
+
+        //end file write
+*/      Log.i(TAG, "OnDestroy accessed");
         unbindService(mConnection);
         super.onDestroy();
     }
